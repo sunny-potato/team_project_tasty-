96dd2e76f973bf0668ab179d818757bf0e1fa88b
sivertbs <sivertbs@stud.ntnu.no>
2022-11-05 14:28:10 +0100

    Updated test for Home page to include list of buttons

--- client/test/pages.test.tsx
+++ client/test/pages.test.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
-import { Recipe, RecipeInfo, Ingredient } from '../src/DataService';
+import dataService, { Recipe, RecipeInfo, Ingredient } from '../src/DataService';
 import { shallow } from 'enzyme';
-import { NavLink } from 'react-router-dom';
+import { MemoryRouter, NavLink } from 'react-router-dom';
 import { Home } from '../src/pages/Home';
 import { DisplayOne } from '../src/pages/DisplayOne';
 import { EditRecipes } from '../src/pages/EditRecipes';
 import Explore from '../src/pages/Explore';
 import { ShoppingCart } from '../src/pages/ShoppingCart';
 import { CreateNew } from '../src/pages/CreateNew';
+
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+let container: any = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 jest.mock('../src/DataService', () => {
   class DataService {
@@ -123,12 +140,38 @@ jest.mock('../src/DataService', () => {
 });
 
 describe('Home page tests', () => {
-  test('Home page draws correctly', (done) => {
+  test('Home page includes correct static elements', (done) => {
     const wrapper = shallow(<Home />);
     setTimeout(() => {
-      expect(wrapper).toMatchSnapshot();
+      expect(
+        wrapper.containsAllMatchingElements([
+          <h1>Welcome to the Tasty</h1>,
+          <p>
+            Here you can explore your own recipes by adding new content, editing existing ones and
+            deleting the ones you don't care about anymore.
+          </p>,
+          <p>
+            This is also the place to discover more and get inspiration from, we have selected a few
+            to show you from [External API] below.
+          </p>,
+          <div className="Content-second">
+            <h5>[Place for external API recipes here]</h5>
+          </div>,
+        ])
+      ).toEqual(true);
       done();
     });
+  });
+  test('Home page draws correctly (snapshot) including list of buttons', async () => {
+    await act(async () => {
+      await render(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>,
+        container
+      );
+    });
+    expect(container).toMatchSnapshot();
   });
 });
 
