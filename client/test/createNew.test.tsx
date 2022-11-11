2542b6354d088a64d9c9d9e3c0ad2fa30e8a5863
sivertbs <sivertbs@stud.ntnu.no>
2022-11-11 11:35:47 +0100

    updated tests for create new

--- client/test/createNew.test.tsx
+++ client/test/createNew.test.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { CreateNew } from '../src/pages/CreateNew';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { DisplayOne } from '../src/pages/DisplayOne';
+import { MemoryRouter, Routes, Route, useNavigate } from 'react-router-dom';
 
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { fireEvent } from '@testing-library/react';
 
-import { Ingredient } from '../src/DataService';
+import dataService, { Ingredient } from '../src/DataService';
 
 let container: any = null;
 beforeEach(() => {
@@ -49,11 +50,11 @@ describe('CreateNew page tests', () => {
     // set input name, and check if value gets updated
     let name = container.querySelector("input[name='name']");
     expect(name.value).toEqual('');
-    fireEvent.change(name, {target: {value: 'New recipe'}});
+    fireEvent.change(name, { target: { value: 'New recipe' } });
     expect(name.value).toEqual('New recipe');
 
     // select meal type 'dinner' (first button) and check if active tag gets updated and background color changes
-    let dinnerButton = container.querySelector("button");
+    let dinnerButton = container.querySelector('button');
     expect(dinnerButton.style.backgroundColor).toEqual('white');
     fireEvent.click(dinnerButton);
     expect(dinnerButton.style.backgroundColor).toEqual('lightblue');
@@ -61,12 +62,62 @@ describe('CreateNew page tests', () => {
     // write desctiption, and check that value gets updated
     let desctiption = container.querySelector("textarea[name='description']");
     expect(desctiption.value).toEqual('');
-    fireEvent.change(desctiption, {target: {value: 'New description'}});
+    fireEvent.change(desctiption, { target: { value: 'New description' } });
     expect(desctiption.value).toEqual('New description');
-
   });
 
   test('Input ingredients', async () => {
-    
-  })
+    await act(async () => {
+      await render(
+        <MemoryRouter>
+          <CreateNew />
+        </MemoryRouter>,
+        container
+      );
+    });
+
+    // set amount and check if value gets updated
+    let amount1 = container.querySelector("input[name='amount']");
+    expect(amount1.value).toEqual('0');
+    fireEvent.change(amount1, { target: { value: 4 } });
+    expect(amount1.value).toEqual('4');
+
+    // select unit and check if it gets updated
+    let unit1 = container.querySelector("select[name='unit']");
+    expect(unit1.value).toEqual('');
+    fireEvent.change(unit1, { target: { value: 'cup' } });
+    expect(unit1.value).toEqual('cup');
+
+    // set name of ingredient and check that value gets updated
+    let name1 = container.querySelector("input[name='ingredient']");
+    expect(name1.value).toEqual('');
+    fireEvent.change(name1, { target: { value: 'new ingredient' } });
+    expect(name1.value).toEqual('new ingredient');
+  });
+
+  
+  test('Save ingredient', async () => {
+    await act(async () => {
+      await render(
+        <MemoryRouter>
+          <CreateNew />
+        </MemoryRouter>,
+        container
+      );
+    });
+
+    // insert required values for new recipe
+    // set name
+    fireEvent.change(container.querySelector("input[name='name']"), {
+      target: { value: 'New recipe' },
+    });
+    // set name of ingredient
+    fireEvent.change(container.querySelector("input[name='ingredient']"), {
+      target: { value: 'whole milk' },
+    });
+    // save recipe
+    fireEvent.click(container.querySelector("button[type='submit']"));
+
+    // expect ??
+  });
 });
--- client/test/mocks/dataServiceMock.tsx
+++ client/test/mocks/dataServiceMock.tsx
@@ -6,70 +6,93 @@ jest.mock('../../src/DataService', () => {
   class DataService {
     /* Get a specific recipe with known id */
     get(id: string) {
-      return Promise.resolve({
-        recipeInfo: {
-          id: 1,
-          name: 'One-Pot Mac and Cheese',
-          meal_type: 'Dinner',
-          new: true,
-          popular: true,
-          description: `Who likes cleaning up after making mac and cheese? 
+      // if testing new recipe
+      if (id === '3') {
+        return Promise.resolve({
+          recipeInfo: {
+            id: 3,
+            name: 'New recipe',
+            meal_type: '',
+            new: true,
+            popular: false,
+            description: '',
+          },
+          ingredients: [
+            {
+              ingredients_id: 1,
+              ingredient: 'whole milk',
+              amount: 0.0,
+              unit_id: 1,
+              unit: '',
+            },
+          ],
+        });
+      } else { // else return test recipe 1
+        return Promise.resolve({
+          recipeInfo: {
+            id: 1,
+            name: 'One-Pot Mac and Cheese',
+            meal_type: 'Dinner',
+            new: true,
+            popular: true,
+            description: `Who likes cleaning up after making mac and cheese? 
               Not this girl. This one-pot mac and cheese is a family favorite, 
               and my 3-year-old is thrilled to see it coming to the dinner table. 
               We love to add sliced smoked sausage to this creamy mac recipe!`,
-        },
-        ingredients: [
-          {
-            ingredients_id: 1,
-            ingredient: 'whole milk',
-            amount: 1.5,
-            unit_id: 2,
-            unit: 'cup',
           },
-          {
-            ingredients_id: 2,
-            ingredient: 'water',
-            amount: 3,
-            unit_id: 2,
-            unit: 'cup',
-          },
-          {
-            ingredients_id: 3,
-            ingredient: 'elbow macaroni (16 ounces)',
-            amount: 1,
-            unit_id: 3,
-            unit: 'package',
-          },
-          {
-            ingredients_id: 4,
-            ingredient: 'velveeta, cubed',
-            amount: 4,
-            unit_id: 4,
-            unit: 'ounce',
-          },
-          {
-            ingredients_id: 5,
-            ingredient: 'shredded sharp cheddar cheese',
-            amount: 2,
-            unit_id: 2,
-            unit: 'cup',
-          },
-          {
-            ingredients_id: 6,
-            ingredient: 'salt',
-            amount: 0.5,
-            unit_id: 5,
-            unit: 'teaspoon',
-          },
-          {
-            ingredients_id: 7,
-            ingredient: 'coarsely ground pepper',
-            amount: 0.5,
-            unit_id: 5,
-            unit: 'teaspoon',
-          },
-        ],
-      });
+          ingredients: [
+            {
+              ingredients_id: 1,
+              ingredient: 'whole milk',
+              amount: 1.5,
+              unit_id: 2,
+              unit: 'cup',
+            },
+            {
+              ingredients_id: 2,
+              ingredient: 'water',
+              amount: 3,
+              unit_id: 2,
+              unit: 'cup',
+            },
+            {
+              ingredients_id: 3,
+              ingredient: 'elbow macaroni (16 ounces)',
+              amount: 1,
+              unit_id: 3,
+              unit: 'package',
+            },
+            {
+              ingredients_id: 4,
+              ingredient: 'velveeta, cubed',
+              amount: 4,
+              unit_id: 4,
+              unit: 'ounce',
+            },
+            {
+              ingredients_id: 5,
+              ingredient: 'shredded sharp cheddar cheese',
+              amount: 2,
+              unit_id: 2,
+              unit: 'cup',
+            },
+            {
+              ingredients_id: 6,
+              ingredient: 'salt',
+              amount: 0.5,
+              unit_id: 5,
+              unit: 'teaspoon',
+            },
+            {
+              ingredients_id: 7,
+              ingredient: 'coarsely ground pepper',
+              amount: 0.5,
+              unit_id: 5,
+              unit: 'teaspoon',
+            },
+          ],
+        });
+      }
     }
 
     // Get an array of all recipes
@@ -117,24 +140,45 @@ jest.mock('../../src/DataService', () => {
     // Get all ingredients
     getAllIngredients() {
       return Promise.resolve([
-        {id: 1, ingredient: 'whole milk'},
-        {id: 2, ingredient: 'water'}
+        { id: 1, ingredient: 'whole milk' },
+        { id: 2, ingredient: 'water' },
       ]);
     }
 
     // Create new ingredient
-    createIngredient(ingredient: string){
+    createIngredient(ingredient: string) {
       return Promise.resolve(8);
     }
 
     // Get all units
     getAllUnits() {
       return Promise.resolve([
-        {id: 1, unit: 'cup'},
-        {id: 2, unit: 'package'}
-      ])
+        { id: 1, unit: 'cup' },
+        { id: 2, unit: 'package' },
+      ]);
     }
 
+    //External API ------------>
+
+    //get key to Explore
+    apiExploreKey() {
+      return Promise.resolve('key');
+    }
+    
+    //get key to Home
+    apiHomeKey() {
+      return Promise.resolve('key');
+    }
+    
+    //get data to Explore
+    apiExploreData(data: any) {
+      return Promise.resolve();
+    }
+    
+    //get data to Home
+    apiHomeData(data: any) {
+      return Promise.resolve();
+    }
     
   }
   return new DataService();
